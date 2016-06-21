f2a4bae6ea834b17a599eebc9f77a5257fc90a64
Nitin Darekar <nvdarekar@gmail.com>
2016-06-21 12:28:30 +0530

    Add heroku profile, node package.json and change app port to deploy on heroku

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
+import os
 from app_orbit_assignment import app
 
 if __name__ == "__main__":
-    app.run()
-#app.run(debug=True)
+    port = int(os.environ.get('PORT', 33507))
+    app.run(port=port)
